6d29c07acd8b68d17eb4a55e0b16c8d0d2b94ac4
Alex Ward <alxwrd@googlemail.com>
2018-04-11 23:54:21 +0100

    add token retrieval

---
+++ specsavers/__init__.py
@@ -0,0 +1,14 @@
+from requests_html import HTMLSession
+
+
+def __get_token():
+    page = HTMLSession().get("https://www.specsavers.co.uk/book/nottingham")
+
+    script_tags = page.html.find("script")
+
+    token_script = [
+            element for element in script_tags
+            if "data-integrity" in element.attrs]
+
+    if token_script:
+        return token_script[0].attrs["data-integrity"]
