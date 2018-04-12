4ce5cbfe9964c310825f8248cc9b1d3e9bd4ab2b
Alex Ward <alxwrd@googlemail.com>
2018-04-12 19:39:41 +0100

    add store class

--- specsavers/api.py
+++ specsavers/api.py
@@ -1,27 +1,61 @@
+import requests
 from requests_html import HTMLSession
 
 
+def retry_on_token_failure(func):
+    def wrapper(*args):
+        try:
+            result = func(*args)
+        except AuthenticationError:
+            api = args[0]
+            api.token = api.fetch_token()
+            result = func(*args)
+        return result
+    return wrapper
+
+
+class AuthenticationError(Exception):
+    ...
+
+
 class Api:
-    base_url = "http://www.specsavers.co.uk"
+    base_url = "https://www.specsavers.co.uk"
 
     def __init__(self):
-        self.__token = ""
+        self.token = self.fetch_token()
 
-    @property
-    def token(self):
-        if not self.__token:
-            self.__token = self.__fetch_token()
-        return self.__token
-
-    def __fetch_token(self):
+    def fetch_token(self):
         page = HTMLSession().get(f"{self.base_url}/book/nottingham")
 
         script_tags = page.html.find("script")
 
         token_script = [
-                element for element in script_tags
-                if "data-integrity" in element.attrs]
+            element for element in script_tags
+            if "data-integrity" in element.attrs]
 
         if not token_script:
             return ""
         return token_script[0].attrs["data-integrity"]
+
+    def store_exists(self, store_name):
+        store_page = requests.head(f"{self.base_url}/book/{store_name}")
+
+        if store_page.status_code == 200:
+            return True
+        else:
+            return False
+
+    @retry_on_token_failure
+    def fetch_store_details(self, store_name):
+        url = (f"{self.base_url}/appointment/api/v1/store"
+               f"?url-name={store_name}")
+
+        store_details = requests.get(
+                url, headers={"X-Access-Token": self.token})
+
+        if store_details.status_code == 401:
+            raise AuthenticationError("Unable to authenticate.")
+        try:
+            return store_details.json()
+        except json.decoder.JSONDecodeError:
+            return {}
---
+++ specsavers/store.py
@@ -0,0 +1,51 @@
+import requests
+import specsavers
+
+from specsavers.api import Api
+
+
+class Store:
+    api = Api()
+
+    def __init__(self, store_name, from_search=False):
+        store_name = store_name.lower()
+
+        if not from_search:
+            if not self.api.store_exists(store_name):
+                raise LookupError(f"The store '{store_name}' is not valid.")
+
+        self.name = store_name
+
+        self.json = self.__fetch_store_details() if not from_search else {}
+
+        self.id = self.json.get("epos")
+        self.full_name = self.json.get("name")
+        self.type = self.json.get("business_type")
+        self.phone = self.json.get("phone", {}).get("number")
+        self.address = self.json.get("address")
+        self.coordinate = self.json.get("coordinate")
+
+    def __getattr__(self, attr):
+        if not self.__dict__["json"]:
+            self.__dict__["json"] = self.__fetch_store_details()
+        try:
+            return self.__dict__[attr]
+        except KeyError:
+            raise AttributeError(
+                    (f"{self.__class__.__name__} object "
+                     f"has no attribute '{attr}'"))
+
+    def __fetch_store_details(self):
+        details = self.api.fetch_store_details(self.name)
+
+        stores = details.get("content", {}).get("stores", [])
+
+        if not stores:
+            return {}
+
+        return stores[0]
+
+
+class StoreList:
+
+    def __init__(self): ...
