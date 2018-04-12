fc4bf8396a6eb2640b0e4fb351ad6b8590aba200
Alex Ward <alxwrd@googlemail.com>
2018-04-12 15:29:57 +0100

    add locate and find methods

--- specsavers/__init__.py
+++ specsavers/__init__.py
@@ -1,5 +1,8 @@
 from requests_html import HTMLSession
 
+from specsavers.store import Store
+from specsavers.store import StoreList
+
 
 def __get_token():
     page = HTMLSession().get("https://www.specsavers.co.uk/book/nottingham")
@@ -12,6 +15,14 @@ def __get_token():
 
     if token_script:
         return token_script[0].attrs["data-integrity"]
+
+
+def locate(coords):
+    return StoreList(coords)
+
+
+def find(store_name):
+    return Store(store_name)
 
 
 TOKEN = __get_token()
