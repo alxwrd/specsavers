318421431f28bde3f83cb5396b03925f7586a3f5
Alex Ward <alxwrd@googlemail.com>
2018-04-13 19:51:35 +0100

    add tests

---
+++ tests/mock_api.py
@@ -0,0 +1,46 @@
+
+
+class MockApi:
+
+    def store_exists(self, store_name):
+        return True
+
+    def fetch_store_details(self, store_name):
+        details = {
+                'code': 200,
+                'locale': 'gb_en',
+                'content': {
+                    'stores': [
+                        {
+                            'epos': 40,
+                            'name': 'Nottingham - Wheelergate',
+                            'business_type': 'opticians',
+                            'url_name': 'nottingham',
+                            'phone': {
+                                'dialing_code': '+44',
+                                'number': '0115 958 8361'
+                                },
+                            'address': {
+                                'line1': '20-24 Wheeler Gate',
+                                'line2': '',
+                                'line3': '',
+                                'town': 'Nottingham',
+                                'county': 'Nottinghamshire',
+                                'post_code': 'NG1 2ND'
+                                },
+                            'booking': {
+                                'engine': 'sas',
+                                'status': 'online'
+                                },
+                            'coordinate': {
+                                'longitude': -1.149914,
+                                'latitude': 52.952229
+                                }
+                            }
+                        ]
+                    }
+                }
+        return details
+
+    def list_of_store_names(self, latitude, longitude):
+        return ["nottingham", "manchester"]
---
+++ tests/test_store.py
@@ -0,0 +1,61 @@
+import unittest
+
+from tests import MockApi
+
+
+class TestStore(unittest.TestCase):
+
+    def setUp(self):
+        import specsavers
+
+        self.specsavers = specsavers
+
+        self.specsavers.Store.api = MockApi
+        self.specsavers.StoreList.api = MockApi
+
+    def test_no_fetching_details_on_search(self):
+        store = self.specsavers.Store("nottingham", from_search=True)
+
+        self.assertEqual(store.json, {})
+
+    def test_details_fetched_once_attr_accessed(self):
+        store = self.specsavers.Store("nottingham", from_search=True)
+
+        store.business_type
+
+        self.assertEqual(store.business_type, "opticians")
+
+    def test_find_gets_store(self):
+        store = self.specsavers.find("nottingham")
+
+        self.assertIsInstance(store, self.specsavers.Store)
+
+    def test_locate_gets_store_list(self):
+        store_list = self.specsavers.locate(
+                latitude=51.507879, longitude=0.087732)
+
+        self.assertIsInstance(store_list, self.specsavers.StoreList)
+
+    def test_store_list_indexable(self):
+        store_list = self.specsavers.locate(
+                latitude=51.507879, longitude=0.087732)
+
+        store = store_list[0]
+
+        self.assertIsInstance(store, self.specsavers.Store)
+
+    def test_store_list_sliceable(self):
+        store_list = self.specsavers.locate(
+                latitude=51.507879, longitude=0.087732)
+
+        store_slice = store_list[0:1]
+
+        self.assertIsInstance(store_slice, self.specsavers.StoreList)
+
+    def test_store_list_lookupable(self):
+        store_list = self.specsavers.locate(
+                latitude=51.507879, longitude=0.087732)
+
+        store = store_list["strand"]
+
+        self.assertIsInstance(store, self.specsavers.Store)
