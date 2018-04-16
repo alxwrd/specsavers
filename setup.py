33c777ab05e407a73e5921168e1d1cc3c6535516
Alex Ward <alxwrd@googlemail.com>
2018-04-16 21:44:40 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='specsavers',
+    packages=['specsavers'],
+    version='',
+    description='A Python wrapper around the Specsavers appointment booking API',
+    author='Alex Ward',
+    author_email='alxwrd@googlemail.com',
+    url='https://github.com/alxwrd/specsavers',
+    long_description=open('README.md').read(),
+    install_requires=['requests', 'maya', 'requests-html'],
+    license='MIT',
+    python_requires='>=3.6',
+)
